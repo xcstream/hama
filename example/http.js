50ae07d6e5f310c45c0f31887d0dc42bce2681b5
xcstream <xcstream@vip.qq.com>
2017-03-12 01:25:21 +0800

    intent

--- example/http.js
+++ example/http.js
@@ -1,7 +1,7 @@
 const http = require('http');
 const server = http.createServer((req, res) => {
-        res.writeHeader(200, {'Content-Type':'text/html;charset=UTF-8'})
-        res.end('还是要学习一个')
+    res.writeHeader(200, {'Content-Type':'text/html;charset=UTF-8'})
+    res.end('还是要学习一个')
 });
 server.listen(8000);
 console.log('http://localhost:8000')
